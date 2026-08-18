6a95b80d6f0e82b14c1b8aa3a8266b3921ca817d
ItsEnd <waweda299@gmail.com>
2026-08-18 16:45:37 +0300

    Сделал задание 2

---
+++ Tasks/Task2/Server.cs
@@ -0,0 +1,47 @@
+﻿namespace Task2;
+
+public static class Server
+{
+    private static int _count = 0;
+    private static readonly ReaderWriterLockSlim _lock = new();
+
+    public static int GetCount()
+    {
+        _lock.EnterReadLock();
+        try
+        {
+            return _count;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
+
+    public static void AddToCount(int count)
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            _count += count;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    //Для тестов
+    public static void Reset() 
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            _count = 0;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+}
--- Tests/Task2/Task2Tests/UnitTest1.cs
+++ Tests/Task2/Task2Tests/UnitTest1.cs
@@ -1,10 +1,45 @@
+using Task2;
+
 namespace Task2Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void AddToCount_ParallelCalls_ShouldNotLoseUpdates()
     {
-        Assert.True(true);
+        Server.Reset();
+
+
+        Parallel.For(0, 100_000, _ =>
+        {
+            Server.AddToCount(1);
+        });
+
+        Assert.Equal(100_000, Server.GetCount());
+    }
+
+    [Fact]
+    public void Server_ShouldHandleConcurrentReadsAndWrites()
+    {
+        Server.Reset();
+
+        const int writers = 10_000;
+        const int readers = 10_000;
+
+        Parallel.Invoke(() =>
+        {
+            Parallel.For(0, writers, _ =>
+            {
+                Server.AddToCount(1);
+            });
+        }, () =>
+        {
+            Parallel.For(0, readers, _ =>
+            {
+                _ = Server.GetCount();
+            });
+        });
+
+        Assert.Equal(writers, Server.GetCount());
     }
 }
